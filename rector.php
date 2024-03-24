8fb6428842c6245172f87b116c112f2d91554991
Andrei Ciungulete <andrei.ciungulete@andali.ro>
2024-03-24 14:55:35 +0200

    Upgrade rector package and refactor config setup
    Updated the "rector/rector" dependency in composer.json to the latest version, 1.0.3. The configuration setup in rector.php was also refactored, improving code readability and maintainability. The configuration now uses fluent interface for a more expressive setup and clear code structure.

--- rector.php
+++ rector.php
@@ -4,27 +4,18 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__.'/src',
-    ]);
-
-    $rectorConfig->skip([
-        __DIR__.'/src/Contracts/Response.php',
-    ]);
-
-    $rectorConfig->rules([
+return RectorConfig::configure()
+    ->withPaths([__DIR__.'/src'])
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class,
         InlineConstructorDefaultToPropertyRector::class,
-    ]);
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-    ]);
-};
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+    )->withPhpSets();
